5468d6d88012458a2504682b2ff8428f13544197
coryellenberger <cory.ellen.ce@gmail.com>
2017-02-08 11:18:22 -0800

    - app.module.js  -- finishing up all the documentation for the properties of the application  -- adding links to all the libraries/related documentation files

--- public/assets/app.module.js
+++ public/assets/app.module.js
@@ -1,5 +1,9 @@
 /**
- * Application Bouquet Angular dependencies
+ * Bouquet.IO Application
+ * @prop {object} ngRoute - The [ngRoute]{@link https://docs.angularjs.org/api/ngRoute} module provides routing and deeplinking services and directives for AngularJS apps
+ * @prop {object} ngResource - The [ngResource]{@link https://docs.angularjs.org/api/ngResource} module provides interaction support with RESTful services
+ * @prop {object} jQuery - [jQuery]{@link globals/jquery.factory.js} Library provided as Angular Dependency
+ * @prop {object} lodash - [Lodash]{@link globals/lodash.factory.js} Library provided as Angular Dependency
  * @module bouquet
  */
 (function () {
